a6d783f07dc0ad5ab1354bef3d3f6b872b3682d9
UlrikWiberg <ulrikw@hiof.no>
2025-05-13 20:23:31 +0200

    homepage ferdig evntpage startet

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -1,44 +1,97 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ArtistCard from "./ArtistCard";
-import "../styles/EventPage.scss"
+import EventCard from "./EventCard"; // For visning av relaterte events
+import "../styles/EventPage.scss";
 
 export default function EventPage() {
-  const { id } = useParams();
-  const [event, setEvent] = useState();
+  const { id } = useParams(); // dette er en event-ID
+  const [event, setEvent] = useState(null);
   const [artists, setArtists] = useState([]);
+  const [tickets, setTickets] = useState([]);
 
-  const getEvent = async () => {
-    try {
-      const res = await fetch(
-        `https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=UlHJiRQNsyx9GOXAmsHGHRSHkLdjsLJv`
-      );
-      const data = await res.json();
-      setEvent(data);
+const getEvent = async () => {
+  try {
+    const res = await fetch(
+      `https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=UlHJiRQNsyx9GOXAmsHGHRSHkLdjsLJv`
+    );
+    const data = await res.json();
+    setEvent(data);
 
-      const attractions = data._embedded?.attractions || [];
-      setArtists(attractions);
-    } catch (error) {
-      console.error("Feil under eventfetch", error);
-    }
-  };
+    const attractions = data._embedded?.attractions || [];
+    setArtists(attractions);
+
+  } catch (error) {
+    console.error("Feil under eventfetch", error);
+  }
+};
+
+const getTickets = async () => {
+    fetch(`https://app.ticketmaster.com/discovery/v2/attractions.json?&locale=*&id=K8vZ917K7fV&id=K8vZ917_YJf&id=K8vZ917oWOV&id=K8vZ917bJC7&apikey=UlHJiRQNsyx9GOXAmsHGHRSHkLdjsLJv`)
+    .then((response) => response.json())
+    .then((data) => setTickets(data._embedded.attractions))
+    .catch((error) => console.error("skjedde feil ved fetch", error))
+};
+
+  
 
   useEffect(() => {
     getEvent();
   }, [id]);
 
+  useEffect(() => {
+    getTickets();
+  }, []);
+
   return (
     <>
-      {artists.length > 0 ? (
-        <section className="artists">
-          {artists.map((artist) => (
-            <ArtistCard artist={artist} key={artist.id} />
-          ))}
-        </section>
+      {!event ? (
+        <p>Laster event...</p>
       ) : (
-        <p>Laster artister...</p>
+        <>
+          <h1>{event._embedded.attractions?.[0]?.name}</h1>
+          <img
+            src={event.images?.[0]?.url}
+            alt={event.name}
+            className="event-img"
+          />
+          <h2>Sjanger: {event.classifications?.[0]?.genre?.name || "Ukjent"}</h2>
+
+          <h2>Følg oss på sosiale medier:</h2>
+
+          <h2>Festivalpass</h2>
+          {tickets.length > 0 ? (
+            <section className="tickets">
+                {tickets.map((ticket) => (
+                    <article className="ticketcard" key={ticket.id}>
+                        <img src={ticket.images?.[0]?.url} alt={ticket.name} />
+                        <h3>{ticket.name}</h3>
+                        <p>{new Date(ticket.dates?.start?.dateTime).toLocaleString("no-NO")}</p>
+                    </article>
+                ))}
+            </section>
+          ) : (
+            <p>Ingen biletter funnet</p>
+          )
+        }
+
+          {artists.length > 0 ? (
+            <section className="artists">
+                <h2>Artister:</h2>
+              {artists.map((artist) => (
+                <ArtistCard artist={artist} key={artist.id} />
+              ))}
+            </section>
+          ) : (
+            <p>Laster artister...</p>
+          )}
+        </>
       )}
     </>
   );
 }
+
+
+
+
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,32 @@
 import { useEffect, useState } from "react";
 import "../styles/Home.scss"
 import { Link } from "react-router-dom";
+import EventCard from "./EventCard";
 
-export default function Home() {
+export default function Home({ events }) {
     const [mainAttractions, setMainAttractions] = useState([]);
+    const [city, setCity] = useState(null);
+    const [cityEvents, setCityEvents] = useState([]);
+
+    const getMainAttractions = async () => {
+        fetch(`https://app.ticketmaster.com/discovery/v2/events?apikey=UlHJiRQNsyx9GOXAmsHGHRSHkLdjsLJv&locale=*&id=Z698xZb_Z16v7eGkFy&id=Z698xZb_Z17q339&id=Z698xZb_Z17qfao&id=Z698xZb_Z16vfkqIjU`)
+        .then((response) => response.json())
+        .then((data) => setMainAttractions(data._embedded?.events))
+        .catch((error) => console.error("skjedde feil i fetch av main", error))
+    };
+
+    const getCityEvents = async (cityName) => {
+        try {
+            const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=UlHJiRQNsyx9GOXAmsHGHRSHkLdjsLJv&locale=*&city=${cityName}`)
+            const data = await response.json();
+            setCityEvents(data._embedded?.events || []);
+        } catch (error) {
+            console.error("feil ved fetch av by eventer", error);
+        }
+    }
+
 //"K8vZ917K7fV"
-    const attractionIds = [
+ /*   const attractionIds = [
         "Z698xZb_Z16v7eGkFy", 
         "Z698xZb_Z17q339", 
         "Z698xZb_Z17qfao",
@@ -23,14 +44,25 @@ export default function Home() {
         } catch (error) {
             console.error("Feil ved henting av attraksjoner:", error);
         }
-    };
+    }; */
 
     useEffect(() => {
         getMainAttractions();
     }, []);
 
+    useEffect(() => {
+        if (city) {
+            getCityEvents(city);
+        }
+    }, [city])
+
+    useEffect(() => {
+        console.log(mainAttractions)
+    }, [mainAttractions])
+
     return (
         <main>
+            <h1>Sommerens festivaler!</h1>
             {mainAttractions?.map((attraction) => (
                 <article key={attraction.id} className="hovedattraksjoner">
                     <h3>{attraction.name}</h3>
@@ -39,6 +71,25 @@ export default function Home() {
                     <Link to={`event/${attraction.id}`}>Les mer her</Link>
                 </article>
             ))}
+            <article className="bysortering">
+                <h2>Hva skjer i verdens storbyer?</h2>
+                <button onClick={() => setCity("Oslo")}>Oslo</button>
+                <button onClick={() => setCity("Stockholm")}>Stockholm</button>
+                <button onClick={() => setCity("Berlin")}>Berlin</button>
+                <button onClick={() => setCity("London")}>London</button>
+                <button onClick={() => setCity("Paris")}>Paris</button>
+            </article>
+            <article className="hjemmeside">
+                <h2>Hva skjer i {city}?</h2>
+                {cityEvents.length > 0 ? (
+                    cityEvents.map((event) => (
+                        <EventCard key={event.id} event={event} />
+                    ))
+                ) : (
+                    <p>Finner ingen eventer</p>
+                )
+            }
+            </article> 
         </main>
     );
 }
