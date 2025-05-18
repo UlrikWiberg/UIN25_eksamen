9fb648c2f541400d0bab735b0677803c2020792d
UlrikWiberg <ulrikw@hiof.no>
2025-05-19 01:27:28 +0200

    sanity slettet

--- src/sanity/client.js
+++
@@ -1,8 +0,0 @@
-import createClient from "@sanity/client"
-
-export const client = createClient({
-    projectId: "phh1hdr7",
-    dataset: "production",
-    apiVersion: "v2025-05-02",
-    useCdn: false,
-})
